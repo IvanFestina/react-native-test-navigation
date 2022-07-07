28b5d20322e9d4bf09747054892b19e9c2b68e49
ivanfestina2 <ivanfestina@gmail.com>
2022-07-07 18:44:05 +0300

    building simple navigation

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,15 +1,56 @@
 import {Button, Text, View} from "react-native";
-import {useAppNavigation} from "../types";
+import {RootType, useAppNavigation} from "../types";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
-export function HomeScreen() {
+const Stack = createNativeStackNavigator<RootType>();
+
+const LoginScreen = () => {
     const navigation = useAppNavigation()
     return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Home Screen</Text>
-            <Button title={'To Users screen'} onPress={() => navigation.navigate('Users', {id: 1000, name: 'Sergey'})}>
-            </Button>
-            <Button title={'To Details screen'} onPress={() => navigation.navigate('Details')}>
-            </Button>
+        <View>
+            <Text>Login Screen</Text>
+            <Button title={'To Registration screen'} onPress={() => navigation.navigate('Home', {screen: 'Registration'})}/>
+            //мы сначала переходим на screen 'Home', а потом переходим на скрин Логин. Вложенная навигация.
+            <Button title={'To Forgot screen'} onPress={() => navigation.navigate('Home', {screen: 'Forgot'})}/>
         </View>
+    )
+}
+const RegistrationScreen = () => {
+    const navigation = useAppNavigation()
+    return (
+        <View>
+            <Text>Registration Screen</Text>
+            <Button title={'To Login screen'} onPress={() => navigation.navigate('Home', {screen: 'Login'})}/>
+        </View>
+    )
+}
+
+const ForgotScreen = () => {
+    const navigation = useAppNavigation()
+
+    return (
+        <View>
+            <Text>ForgotScreen</Text>
+            <Button title={'To Login screen'} onPress={() => navigation.navigate('Home', {screen: 'Login'})}/>
+        </View>
+    )
+}
+
+
+export function HomeScreen() {
+
+    return (
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name={'Login'} component={LoginScreen}/>
+            <Stack.Screen name={'Registration'} component={RegistrationScreen}/>
+            <Stack.Screen name={'Forgot'} component={ForgotScreen}/>
+        </Stack.Navigator>
+        // <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        //     <Text>Home Screen</Text>
+        //     <Button title={'To Users screen'} onPress={() => navigation.navigate('Users', {id: 1000, name: 'Sergey'})}>
+        //     </Button>
+        //     <Button title={'To Details screen'} onPress={() => navigation.navigate('Details')}>
+        //     </Button>
+        // </View>
     );
 }
--- src/screens/types.ts
+++ src/screens/types.ts
@@ -1,11 +1,23 @@
-import {NavigationProp, useNavigation} from "@react-navigation/native";
+import {NavigationProp, useNavigation, NavigatorScreenParams} from "@react-navigation/native";
 import {NativeStackScreenProps} from '@react-navigation/native-stack'
 
 export type RootStackParamList = {
-    Home: undefined;
+    Home: NavigatorScreenParams<RootType>;
+    //у нас есть родительский экран, который содержит в себе под экраны <RootType>
+    //чтобы прыгнуть в экран ребенка Login , мы сначала обращаемся к родителю, Home
+    // который содержит в себя Login, Registration, Forgot
+    // Home => Login
+    //             <Button title={'To Login screen'} onPress={() => navigation.navigate('Home', {screen: 'Login'})}/>
+    //если прыгать еще глубже ('Home', {screen: 'Login', params: {screen: 'Name'})
+
     Users: {id: number, name: string} | undefined;
     Details: undefined;
 };
+export type RootType = {
+    Login: undefined
+    Registration: undefined
+    Forgot: undefined
+}
 
 export type NavigationUseType = NavigationProp<RootStackParamList>
 
@@ -14,3 +26,4 @@ export const useAppNavigation = () => useNavigation<NavigationUseType>()
 export type HomePropsType = NativeStackScreenProps<RootStackParamList, 'Home'>
 export type UsersPropsType = NativeStackScreenProps<RootStackParamList, 'Users'>
 export type DetailsPropsType = NativeStackScreenProps<RootStackParamList, 'Details'>
+
