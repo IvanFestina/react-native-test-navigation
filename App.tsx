c65c0b2aa6e3ba5db186de75568e3448dc4196cc
IvanFestina <ivanfestina@gmail.com>
2022-08-08 17:59:40 +0300

    added login page

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
+import React from 'react';
 import {NavigationContainer} from "@react-navigation/native";
-import React from "react";
-import {Main} from "./src/Main";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {RootMainParamList} from "./src/types/types";
+import LoginScreen from "./src/LoginScreen";
+import {TabList} from "./src/TabList";
 
+const MainStack = createNativeStackNavigator<RootMainParamList>()
 
 export default function App() {
     return (
-        <React.Fragment>
-            <NavigationContainer>
-                <Main/>
-            </NavigationContainer>
-        </React.Fragment>
-    );
-}
+        <NavigationContainer>
+            <MainStack.Navigator initialRouteName={'LoginScreen'} screenOptions={{headerShown: false}}>
+                <MainStack.Screen name={'LoginScreen'} component={LoginScreen}/>
+                <MainStack.Screen name={'TabList'} component={TabList}/>
+            </MainStack.Navigator>
+        </NavigationContainer>
+    )
+};
+
